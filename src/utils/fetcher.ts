d1e11b55733d8f1d189c327826d0592db8abbe0d
adust09 <shoukitsuda@icloud.com>
2021-07-12 17:01:45 +0900

    no message

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,5 +1,4 @@
 import { Octokit } from "@octokit/core";
-
-export const octokit = new  Octokit({
-    auth: process.env.GITHUB_ACCESS_TOKEN,
+export const octokit = new Octokit({
+  auth: process.env.GITHUB_ACCESS_TOKEN,
 });
